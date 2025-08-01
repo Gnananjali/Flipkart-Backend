42442943ce689934874009b007777ad465a8f0f0
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2025-08-01 10:32:54 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const Cart=mongoose.model('Cart',new mongoose.Schema({
     userId:String,
+    status: { type: String, default: 'active' },
+    updatedAt: { type: Date, default: Date.now },
     items:[
         {
             productId:String,
@@ -17,16 +19,16 @@ router.post('/cart/add',async(req,res)=>{
     try{
         const {productId, quantity=1,user} = req.body;
 
-        if(!productId || !user) {
+        if(!productId || !userId) {
             return res.status(400).json({message:"Productid and user is required"});
     }
 
-    let cart=await Cart.findOne({userId:user,status:'active'});
+    let cart=await Cart.findOne({userId,status:'active'});
 
     if(!cart){
         cart=new Cart({userId:user,items:[],status:'active'});
     }
-    const existingItemIndex=cart.items.find(item=>item.productId===productId);
+    const existingItemIndex=cart.items.findIndex(item=>item.productId===productId);
 
     if(existingItemIndex>-1){
         cart.items[existingItemIndex].quantity+=parseInt(quantity);
@@ -38,6 +40,7 @@ router.post('/cart/add',async(req,res)=>{
     }
     cart.updateAt=new Date();
     await cart.save();
+        res.status(200).json({ success: true, message: "Item added to cart", cart });
     }catch (err) {
         res.status(500).json({error:"Internal server error,Item has not been added"});
     }
